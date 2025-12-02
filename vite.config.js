af91a52b1e8ba4bbed24dbe003535860650f2ff8
Khanghcmut <138753176+Khanghcmut@users.noreply.github.com>
2025-12-02 21:04:25 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@
 // })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Only if using the vite plugin approach, otherwise ignore
+// import tailwindcss from '@tailwindcss/vite' // Only if using the vite plugin approach, otherwise ignore
 
 // https://vitejs.dev/config/
 export default defineConfig({
